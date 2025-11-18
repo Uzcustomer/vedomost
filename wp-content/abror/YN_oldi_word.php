bd32c3e94c71ad9c0b225663e9c340a401d6619e
Uzcustomer <148035366+Uzcustomer@users.noreply.github.com>
2025-11-18 19:54:19 +0500

    Add function to render Word document with data

--- wp-content/abror/YN_oldi_word.php
+++ wp-content/abror/YN_oldi_word.php
@@ -24,3 +24,25 @@ $meta = [
 // Word renderer-ni ulash
 require_once __DIR__ . '/YN_oldi_word.php';
 yn_oldi_render_word($rows, $meta);
+if (!function_exists('yn_oldi_render_word')) {
+    function yn_oldi_render_word(array $rows, array $meta = []) {
+        $h = fn($v) => htmlspecialchars((string)$v, ENT_QUOTES, 'UTF-8');
+        header("Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        header("Content-Disposition: attachment; filename=YN_oldi_vedomost.doc");
+        $doc = '';
+        $doc .= '<h2>Yakuniy nazorat oldi VEDOMOSTI</h2>';
+        $doc .= '<p><b>Fakultet:</b> '.$h($meta['faculty_name']??'').'</p>';
+        $doc .= '<p><b>Guruh:</b> '.$h($meta['group_name']??'').'</p>';
+        $doc .= '<p><b>Fan:</b> '.$h($meta['subject_name']??'').'</p>';
+        $doc .= '<p><b>O\'qituvchi:</b> '.$h($meta['teacher_name']??'').'</p>';
+        $doc .= '<p><b>Sana:</b> '.($meta['date']??date('Y-m-d')).'</p>';
+        $doc .= '<table><tbody><tr><th>№</th><th>Talaba F.I.O.</th><th>Guruh</th><th>JN balli</th><th>Imzo</th></tr>';
+        $i=1;
+        if ($rows) foreach ($rows as $r)
+            $doc .= '<tr><td>'.$i++.'</td><td>'.$h($r['student_name']??'').'</td><td>'.$h($r['group_name']??'').'</td><td>'.$h($r['jn_ball']??'').'</td><td></td></tr>';
+        else
+            $doc .= '<tr><td>Maʼlumot topilmadi</td></tr>';
+        $doc .= '</tbody></table><br><table><tbody><tr><td>Kafedra mudiri: _______________________</td><td>O\'qituvchi: _______________________</td></tr></tbody></table>';
+        echo $doc; exit;
+    }
+}
